c5f9acbfa2f917bc31b8fd8597273061389ab777
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-07 13:22:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,36 +13,34 @@ def main():
         # Open the login page
         driver.get("http://172.171.252.63/")
 
-        # Fill in the username and password fields with valid values
-        username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "username")))
-        password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "password")))
+        # Find and interact with the email input field using its XPath
+        email_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="tab1"]/form/div[1]/input'))
+        )
+        email_input.send_keys("pavan.veeramaneni@indiahealthlink.com")
 
-        valid_username = "pavan.veeramaneni@indiahealthlink.com"
-        valid_password = "ABCd@123456"
+        # Find and interact with the password input field using its XPath
+        password_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="tab1"]/form/div[2]/input'))
+        )
+        password_input.send_keys("ABCd@123456")
 
-        username_field.send_keys(valid_username)
-        password_field.send_keys(valid_password)
-
-        # Find and click the login button (Correct the selector)
+        # Find and click the login button using the provided XPath
         login_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="submit"][value="Login"]')
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="tab1"]/form/div[3]/button'))
         )
         login_button.click()
 
-        # Wait for the welcome message element to appear
-        welcome_message_element = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.ID, "welcomeMessage"))
-        )
+        # You can add a delay here to wait for the page to load and check for login success
+        # For example, sleep for a few seconds:
+        import time
+        time.sleep(5)  # Adjust the sleep time as needed
 
-        # Get the text content of the welcome message
-        welcome_message_text = welcome_message_element.find_element(By.TAG_NAME, "h3").text
-
-        # Verify the welcome message
-        expected_welcome_message = "Welcome"
-        if welcome_message_text == expected_welcome_message:
-            print("Test passed! Welcome message displayed.")
+        # Check if the page URL has changed, indicating a successful login
+        if driver.current_url == "URL_AFTER_LOGIN_SUCCESS":
+            print("Test passed! Username and password are correct.")
         else:
-            print(f"Test failed! Expected: '{expected_welcome_message}', Actual: '{welcome_message_text}'")
+            print("Test failed! Username or password is incorrect.")
 
     except Exception as e:
         print("An error occurred:", str(e))
