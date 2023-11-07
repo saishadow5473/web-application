fa228a76395650bb96c4cb328b8fda49ab9e179f
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-07 14:57:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,21 +13,21 @@ def main():
         # Open the login page
         driver.get("http://172.171.252.63/")
 
-        # Find and interact with the email input field using its XPath
-        email_input = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="tab1"]/form/div[1]/input'))
+        # Find and interact with the user ID input field using its XPath
+        user_id_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[1]/input'))
         )
-        email_input.send_keys("pavan.veeramaneni@indiahealthlink.com")
+        user_id_input.send_keys("pavan.veeramaneni@indiahealthlink.com")
 
         # Find and interact with the password input field using its XPath
         password_input = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="tab1"]/form/div[2]/input'))
+            EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[2]/input'))
         )
         password_input.send_keys("ABCd@123456")
 
-        # Find and click the login button using the provided XPath
+        # Find and click the login button using its XPath
         login_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="tab1"]/form/div[3]/button'))
+            EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[3]/button'))
         )
         login_button.click()
 
@@ -37,10 +37,10 @@ def main():
         time.sleep(5)  # Adjust the sleep time as needed
 
         # Check if the page URL has changed, indicating a successful login
-        if driver.current_url == "URL_AFTER_LOGIN_SUCCESS":
-            print("Test passed! Username and password are correct.")
+        if driver.current_url == "http://172.171.252.63/takesurvey":
+            print("Test passed! User ID and password are correct.")
         else:
-            print("Test failed! Username or password is incorrect.")
+            print("Test failed! User ID or password is incorrect.")
 
     except Exception as e:
         print("An error occurred:", str(e))
