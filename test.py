09aa058e422d46fd27ae62751a2b7bde8d1607a2
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-08 18:00:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -15,25 +15,25 @@ def main():
         driver.get("http://172.171.252.63/")
 
         # Find and interact with the user ID input field using its XPath
-        user_id_input = WebDriverWait(driver, 10).until(
+        user_id_input = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[1]/input'))
         )
         user_id_input.send_keys("pavan.veeramaneni@indiahealthlink.com")
 
         # Find and interact with the password input field using its XPath
-        password_input = WebDriverWait(driver, 10).until(
+        password_input = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[2]/input'))
         )
         password_input.send_keys("ABCd@123456")
 
         # Find and click the login button using its XPath
-        login_button = WebDriverWait(driver, 10).until(
+        login_button = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-login/div/div/div[2]/div/div/div/div/div[3]/div/form/div[3]/button'))
         )
         login_button.click()
 
         # Wait for the element on the next page to be displayed
-        success_element = WebDriverWait(driver, 10).until(
+        success_element = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-take-survey/app-sidebar/div/div/a[1]/span'))
         )
 
@@ -41,7 +41,7 @@ def main():
         print("Login successful")
 
         # Wait for the Tele Consultation tab to be displayed
-        tele_consultation_tab = WebDriverWait(driver, 10).until(
+        tele_consultation_tab = WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-take-survey/app-sidebar/div/div/a[2]/span'))
         )
 
