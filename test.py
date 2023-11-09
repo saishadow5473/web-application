b3296550df1a94afa999100b6ee590a75532e0e3
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-09 18:14:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ def main():
 
         # Click on the dashboard icon button
         dashboard_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-teleconsultdashboard/app-sidebar/div/div/a[2]'))
+            EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-dt/app-sidebar/div/div/a[1]/span'))
         )
         dashboard_button.click()
 
@@ -81,7 +81,7 @@ def main():
 
         # Click on the tele consultation tab
         tele_consultation_tab = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-teleconsultdashboard/app-sidebar/div/div/a[2]/span'))
+            EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-teleconsultdashboard/app-sidebar/div/div/a[2]'))
         )
         tele_consultation_tab.click()
 
