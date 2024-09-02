98ddbbdb5fdd9f8489fac3626e5c82fda17f854d
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2024-09-02 12:26:26 +0530

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -9,10 +9,10 @@ def main():
     # Set up the Selenium WebDriver with options
     options = webdriver.ChromeOptions()
     # Comment the next line if you want to see the browser window
-    # options.add_argument('--headless')  # Use this if you're running headless
+    options.add_argument('--headless')  # Use this if you're running headless
     # Actual path to Chrome binary (change this to the path on your machine)
-    # options.binary_location = '/usr/bin/google-chrome'
-    options.binary_location = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    options.binary_location = '/usr/bin/google-chrome'
+    # options.binary_location = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
     driver = webdriver.Chrome(options=options)
 
     try:
